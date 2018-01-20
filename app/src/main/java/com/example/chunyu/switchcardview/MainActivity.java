672a9180adf75cd4ba15c1302fd8c6d8b431c021
chunyu <jcy23401@163.com>
2018-01-20 19:25:09 +0800

    提交Demo页面辅助代码。

--- app/src/main/java/com/example/chunyu/switchcardview/MainActivity.java
+++ app/src/main/java/com/example/chunyu/switchcardview/MainActivity.java
@@ -2,12 +2,57 @@ package com.example.chunyu.switchcardview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.chunyu.switchcardview.adapter.SwitchCardViewAdapter;
+import com.example.chunyu.switchcardview.view.SwitchCardView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mNormalBtn;
+
+    private Button mRecyclerViewBtn;
+
+    private Button mListViewTestBtn;
+
+    SwitchCardView mSwitchCardView;
+
+    SwitchCardViewAdapter mSwitchCardViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void initView() {
+        mNormalBtn = findViewById(R.id.normal_btn);
+        mRecyclerViewBtn = findViewById(R.id.recyclerViewbtn);
+        mListViewTestBtn = findViewById(R.id.listview_btn);
+        mSwitchCardView = findViewById(R.id.switch_cardview);
+
+        mNormalBtn.setOnClickListener(this);
+        mRecyclerViewBtn.setOnClickListener(this);
+        mListViewTestBtn.setOnClickListener(this);
+
+    }
+
+    private void initCardViewAdapter() {
+         mSwitchCardViewAdapter = new SwitchCardViewAdapter();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.normal_btn:
+                break;
+            case R.id.listview_btn:
+                break;
+            case R.id.recyclerViewbtn:
+                break;
+            default:
+                break;
+        }
     }
 }
